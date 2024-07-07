95bed8c9289ddca546b5865ff7f7678ec8762736
yliucam <163459558+yliucam@users.noreply.github.com>
2024-07-07 19:24:28 +0100

    Update dc_smc_util.R

--- dc_smc/dc_smc_util.R
+++ dc_smc/dc_smc_util.R
@@ -1,5 +1,5 @@
-# MCMC/HM kernel help function on the subroot node
-sub_mcmc <- function(data, x_0, prior_alpha, prior_beta, like_beta, Ntotal) {
+# MCMC/MH kernel help function on the subroot node
+sub_mcmc <- function(data, x_0, prior_alpha, prior_beta, like_beta, var_sub, Ntotal) {
   N <- length(x_0)
   
   # Storage
@@ -8,7 +8,8 @@ sub_mcmc <- function(data, x_0, prior_alpha, prior_beta, like_beta, Ntotal) {
   for (i in 1:(Ntotal-1)) {
     if (i == 1) {
       # Proposal - log-normal random walk
-      x_star <- exp(mvrnorm(1, log(x_0), 100*diag(N))) 
+      x_star <- sapply(x_0, function(x) rlnorm(1, log(x), 2.38*sqrt(var_sub)))
+      #x_star <- exp(mvrnorm(1, log(x_0), 2.38*diag(var_sub, N))) 
       
       # Prior
       prior_log <- sapply(x_0, function(x) dgamma(x, prior_alpha, prior_beta, log = T))
@@ -24,8 +25,13 @@ sub_mcmc <- function(data, x_0, prior_alpha, prior_beta, like_beta, Ntotal) {
       post_log <- prior_log + like_log
       post_star_log <- prior_star_log + like_star_log
       
+      # Transition/proposal density
+      x_and_x_star <- cbind(x_0, x_star)
+      prop_x_to_x_star <- apply(x_and_x_star, 1, function(x) dlnorm(x[2], log(x[1]), 2.38*sqrt(var_sub), log = T))
+      prop_x_star_to_x <- apply(x_and_x_star, 1, function(x) dlnorm(x[1], log(x[2]), 2.38*sqrt(var_sub), log = T))
+      
       # Accept/reject - carry out in a matrix format
-      ratio <- post_star_log - post_log + log(x_star) - log(x_0) # Note: consider the Jacobian determinant
+      ratio <- post_star_log - post_log + prop_x_star_to_x - prop_x_to_x_star
       alpha <- sapply(ratio, function(x) min(0, x))
       Unif_rand <- runif(N, 0, 1)
       index <- which(log(Unif_rand) < alpha)
@@ -36,7 +42,8 @@ sub_mcmc <- function(data, x_0, prior_alpha, prior_beta, like_beta, Ntotal) {
     
     
     # Proposal - log-normal random walk
-    x_star <- exp(mvrnorm(1, log(x), 100*diag(N)))
+    x_star <- sapply(x, function(x) rlnorm(1, log(x), 2.38*sqrt(var_sub)))
+    #x_star <- exp(mvrnorm(1, log(x), 2.38*diag(var_sub, N)))
     
     # Prior
     prior_log <- sapply(x, function(x) dgamma(x, prior_alpha, prior_beta, log = T))
@@ -46,10 +53,19 @@ sub_mcmc <- function(data, x_0, prior_alpha, prior_beta, like_beta, Ntotal) {
     data_and_x <- cbind(data, x)
     like_log <- apply(data_and_x, 1, function(x) dbeta(x[1], x[3], like_beta[1], log = T) + dbeta(x[2], x[3], like_beta[2], log = T))
     data_and_x_star <- cbind(data, x_star)
-    like_log <- apply(data_and_x_star, 1, function(x) dbeta(x[1], x[3], like_beta[1], log = T) + dbeta(x[2], x[3], like_beta[2], log = T))
+    like_star_log <- apply(data_and_x_star, 1, function(x) dbeta(x[1], x[3], like_beta[1], log = T) + dbeta(x[2], x[3], like_beta[2], log = T))
+    
+    # Posterior
+    post_log <- prior_log + like_log
+    post_star_log <- prior_star_log + like_star_log
+    
+    # Transition/proposal density
+    x_and_x_star <- cbind(x, x_star)
+    prop_x_to_x_star <- apply(x_and_x_star, 1, function(x) dlnorm(x[2], log(x[1]), 2.38*sqrt(var_sub), log = T))
+    prop_x_star_to_x <- apply(x_and_x_star, 1, function(x) dlnorm(x[1], log(x[2]), 2.38*sqrt(var_sub), log = T))
     
     # Accept/reject - carry out in a matrix format
-    ratio <- post_star_log - post_log + log(x_star) - log(x) # Note: consider the Jacobian determinant
+    ratio <- post_star_log - post_log + prop_x_star_to_x - prop_x_to_x_star
     alpha <- sapply(ratio, function(x) min(0, x))
     Unif_rand <- runif(N, 0, 1)
     index <- which(log(Unif_rand) < alpha)
@@ -63,8 +79,8 @@ sub_mcmc <- function(data, x_0, prior_alpha, prior_beta, like_beta, Ntotal) {
 
 
 
-# MCMC/HM kernel help function on the root node
-root_mcmc <- function(data, x_0, prior_mu, prior_sigma, like_beta, Ntotal) {
+# MCMC/MH kernel help function on the root node
+root_mcmc <- function(data, x_0, prior_mu, prior_sigma, like_beta, var_root, Ntotal) {
   N <- length(x_0)
   
   # Storage
@@ -73,12 +89,13 @@ root_mcmc <- function(data, x_0, prior_mu, prior_sigma, like_beta, Ntotal) {
   for (i in 1:(Ntotal-1)) {
     if (i == 1) {
       # Proposal - log-normal random walk
-      x_star <- exp(mvrnorm(1, log(x_0), 100*diag(N)))
+      x_star <- sapply(x_0, function(x) rlnorm(1, log(x), 2.38*sqrt(var_root)))
+      #x_star <- exp(mvrnorm(1, log(x_0), 10*diag(N)))
       
       
       # Prior
       prior_log <- sapply(x_0, function(x) dlnorm(x, prior_mu, prior_sigma, log = T))
-      prior_star_log <- sapply(x_0, function(x) dlnorm(x, prior_mu, prior_sigma, log = T))
+      prior_star_log <- sapply(x_star, function(x) dlnorm(x, prior_mu, prior_sigma, log = T))
       
       # Likelihood
       data_and_x <- cbind(data, x_0)
@@ -90,8 +107,13 @@ root_mcmc <- function(data, x_0, prior_mu, prior_sigma, like_beta, Ntotal) {
       post_log <- prior_log + like_log
       post_star_log <- prior_star_log + like_star_log
       
+      # Transition/proposal density
+      x_and_x_star <- cbind(x_0, x_star)
+      prop_x_to_x_star <- apply(x_and_x_star, 1, function(x) dlnorm(x[2], log(x[1]), 2.38*sqrt(var_root), log = T))
+      prop_x_star_to_x <- apply(x_and_x_star, 1, function(x) dlnorm(x[1], log(x[2]), 2.38*sqrt(var_root), log = T))
+      
       # Accept/reject - carry out in a matrix form
-      ratio <- post_star_log - post_log + log(x_star) - log(x_0) # Note: consider the Jacobian determinant
+      ratio <- post_star_log - post_log + prop_x_star_to_x - prop_x_to_x_star
       alpha <- sapply(ratio, function(x) min(0, x))
       Unif_rand <- runif(N, 0, 1)
       index <- which(log(Unif_rand) < alpha)
@@ -102,11 +124,12 @@ root_mcmc <- function(data, x_0, prior_mu, prior_sigma, like_beta, Ntotal) {
     
     
     # Proposal - log-normal random walk
-    x_star <- exp(mvrnorm(1, log(x), 100*diag(N)))
+    x_star <- sapply(x, function(x) rlnorm(1, log(x), 2.38*sqrt(var_root)))
+    #x_star <- exp(mvrnorm(1, log(x), 10*diag(N)))
     
     # Prior
     prior_log <- sapply(x, function(x) dlnorm(x, prior_mu, prior_sigma, log = T))
-    prior_star_log <- sapply(x, function(x) dlnorm(x_star, prior_mu, prior_sigma, log = T))
+    prior_star_log <- sapply(x_star, function(x) dlnorm(x, prior_mu, prior_sigma, log = T))
     
     # Likelihood
     data_and_x <- cbind(data, x)
@@ -114,8 +137,17 @@ root_mcmc <- function(data, x_0, prior_mu, prior_sigma, like_beta, Ntotal) {
     data_and_x_star <- cbind(data, x_star)
     like_star_log <- apply(data_and_x_star, 1, function(x) dgamma(x[1], x[3], like_beta[1], log = T) + dgamma(x[2], x[3], like_beta[2], log = T))
     
+    # Posterior
+    post_log <- prior_log + like_log
+    post_star_log <- prior_star_log + like_star_log
+    
+    # Transition/proposal density
+    x_and_x_star <- cbind(x, x_star)
+    prop_x_to_x_star <- apply(x_and_x_star, 1, function(x) dlnorm(x[2], log(x[1]), 2.38*sqrt(var_root), log = T))
+    prop_x_star_to_x <- apply(x_and_x_star, 1, function(x) dlnorm(x[1], log(x[2]), 2.38*sqrt(var_root), log = T))
+    
     # Accept/reject - carry out in a matrix form
-    ratio <- post_star_log - post_log + log(x_star) - log(x) # Note: consider the Jacobian determinant
+    ratio <- post_star_log - post_log + prop_x_star_to_x - prop_x_to_x_star
     alpha <- sapply(ratio, function(x) min(0, x))
     Unif_rand <- runif(N, 0, 1)
     index <- which(log(Unif_rand) < alpha)
