49c0da0a27d9da7e8021667e0a79c17af46cb6d4
rjbgoudie <rjbgoudie@users.noreply.github.com>
2024-08-21 14:55:47 +0100

    Comparing JAGS and DCSMC results
    This renames some of the parameters in the JAGS model to make them easier to process

--- dc_smc/example_BUGS.R
+++ dc_smc/example_BUGS.R
@@ -2,30 +2,25 @@ library(rjags)
 
 hier_bugs <- "model
 {
-  alpha ~ dlnorm(3, 1/4)
-  
-  alpha1 ~ dgamma(alpha, 1)
-  alpha2 ~ dgamma(alpha, .5)
-  
-  p11 ~ dbeta(alpha1, 2)
-  p12 ~ dbeta(alpha1, 2)
-  p13 ~ dbeta(alpha1, 2)
-  p14 ~ dbeta(alpha1, 2)
-  
-  p21 ~ dbeta(alpha2, 10)
-  p22 ~ dbeta(alpha2, 10)
-  p23 ~ dbeta(alpha2, 10)
-  p24 ~ dbeta(alpha2, 10)
+  alpha0 ~ dlnorm(3, 1/4)
+
+  alpha[1] ~ dgamma(alpha0, 1)
+  alpha[2] ~ dgamma(alpha0, .5)
+
+  for (j in 1:4){
+    p[1, j] ~ dbeta(alpha[1], 2)
+    p[2, j] ~ dbeta(alpha[2], 10)
+  }
   
   for (i in 1:20) {
-    y11[i] ~ dbin(p11, 100)
-    y12[i] ~ dbin(p12, 100)
-    y13[i] ~ dbin(p13, 100)
-    y14[i] ~ dbin(p14, 100)
-    y21[i] ~ dbin(p21, 200)
-    y22[i] ~ dbin(p22, 200)
-    y23[i] ~ dbin(p23, 200)
-    y24[i] ~ dbin(p24, 200)
+    y11[i] ~ dbin(p[1,1], 100)
+    y12[i] ~ dbin(p[1,2], 100)
+    y13[i] ~ dbin(p[1,3], 100)
+    y14[i] ~ dbin(p[1,4], 100)
+    y21[i] ~ dbin(p[2,1], 200)
+    y22[i] ~ dbin(p[2,2], 200)
+    y23[i] ~ dbin(p[2,3], 200)
+    y24[i] ~ dbin(p[2,4], 200)
   }
   
 }
@@ -33,7 +28,8 @@ hier_bugs <- "model
 
 data <- list(y11=y11, y12=y12, y13=y13, y14=y14, y21=y21, y22=y22, y23=y23, y24=y24)
 
-parameters <- c('alpha', 'alpha1', 'alpha2', 'p11', 'p12', 'p13', 'p14', 'p21', 'p22', 'p23', 'p24')
+parameters <- c('alpha0', 'alpha','p')
+
 
 burn_in <- 30000
 
@@ -45,15 +41,16 @@ foo <- jags.model(textConnection(hier_bugs), data = data)
 
 update(foo, burn_in)
 
-out <- coda.samples(model=foo, variable.names = parameters, n.iter = steps, thin = thin)
-
-outmatrix <- as.matrix(out)
-
-
-# Double check prior in JAGS model matches the simulated prior
+jags <- coda.samples(model=foo, variable.names = parameters, n.iter = steps, thin = thin)
 
 library(posterior, quietly = TRUE)
 library(tidybayes)
+
+jags_draws <- as_draws(as.matrix(jags))
+summary(jags_draws)
+
+
+# Double check prior in JAGS model matches the simulated prior
 
 no_data <- list()
 prior <- jags.model(textConnection(hier_bugs), data = no_data)
