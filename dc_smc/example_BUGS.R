4a2b19355d57686dfb137cbb515f4b29446fdf5e
Yixuan Liu <yixuan.liu@mrc-bsu.cam.ac.uk>
2024-08-23 13:12:24 +0100

    Try more nodes

--- dc_smc/example_BUGS.R
+++ dc_smc/example_BUGS.R
@@ -2,7 +2,6 @@ library(rjags)
 
 hier_bugs <- "model
 {
-<<<<<<< HEAD
   alpha ~ dunif(.001, 10000)
   
   alpha1 ~ dgamma(alpha, 1)
@@ -16,27 +15,6 @@ hier_bugs <- "model
       y1[j,i] ~ dbin(p1[i], 100)
       y2[j,i] ~ dbin(p2[i], 200)
     }
-=======
-  alpha0 ~ dlnorm(3, 1/4)
-
-  alpha[1] ~ dgamma(alpha0, 1)
-  alpha[2] ~ dgamma(alpha0, .5)
-
-  for (j in 1:4){
-    p[1, j] ~ dbeta(alpha[1], 2)
-    p[2, j] ~ dbeta(alpha[2], 10)
-  }
-  
-  for (i in 1:20) {
-    y11[i] ~ dbin(p[1,1], 100)
-    y12[i] ~ dbin(p[1,2], 100)
-    y13[i] ~ dbin(p[1,3], 100)
-    y14[i] ~ dbin(p[1,4], 100)
-    y21[i] ~ dbin(p[2,1], 200)
-    y22[i] ~ dbin(p[2,2], 200)
-    y23[i] ~ dbin(p[2,3], 200)
-    y24[i] ~ dbin(p[2,4], 200)
->>>>>>> 3d069e7b4bd1c0ceb2d97f856addf6e80895fa2d
   }
   
 }
@@ -44,12 +22,8 @@ hier_bugs <- "model
 
 data <- list(y1=y1, y2=y2)
 
-<<<<<<< HEAD
-parameters <- c('alpha', 'alpha1', 'alpha2')
-=======
-parameters <- c('alpha0', 'alpha','p')
 
->>>>>>> 3d069e7b4bd1c0ceb2d97f856addf6e80895fa2d
+parameters <- c('alpha', 'alpha1', 'alpha2')
 
 burn_in <- 30000
 
