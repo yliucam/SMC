c1c785f35c3cc6352bd9db28838e06fa502e03da
Yixuan Liu <yixuan.liu@mrc-bsu.cam.ac.uk>
2025-12-02 13:33:31 +0000

    update

--- dc_melding/owls/owls_run.R
+++ dc_melding/owls/owls_run.R
@@ -8,7 +8,6 @@ library(matrixStats)
 
 sourceCpp("C:/Users/Yixuan/Documents/codes/SMC/dc_melding/owls/dc_melding_owls_util.cpp")
 source("C:/Users/Yixuan/Documents/codes/SMC/dc_melding/owls/dc_melding_owls_util.R")
-source("C:/Users/Yixuan/Documents/codes/SMC/dc_melding/owls/merging_loglik.R")
 source("C:/Users/Yixuan/Documents/codes/SMC/dc_melding/owls/dc_melding_owls_jags.R")
 source("C:/Users/Yixuan/Documents/codes/SMC/dc_melding/owls/dc_melding_owls.R")
 
